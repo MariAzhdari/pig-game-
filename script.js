779e07abb5b0e306cb20aefc0b8dc8948ae5b05e
Mari Azh <Marzi.aghchay@gmail.com>
2023-01-30 17:31:53 +0000

    when dice is not one

--- script.js
+++ script.js
@@ -2,6 +2,8 @@
 //selections elements
 let score0El = document.getElementById("score--0");
 let score1El = document.getElementById("score--1");
+let current0El = document.getElementById("current--0");
+let current1El= document.getElementById("current--1");
 let diceEl = document.querySelector(".dice");
 let btnNew = document.querySelector(".btn--new");
 let btnRoll = document.querySelector(".btn--roll");
@@ -12,14 +14,24 @@ let btnHold = document.querySelector(".btn--hold");
 score0El.innerText = 0;
 score1El.innerText = 0;
 diceEl.classList.add("hidden")
+let currentScore = 0;
 
 //dice functionality
 btnRoll.addEventListener("click",function(){
     //random number for dices
-    let dice =Math.trunc(Math.random()*6)+1
+    let dice = Math.trunc(Math.random()*6)+1
     console.log(dice);
     //remove the hidden class from diceEl
     diceEl.classList.remove("hidden")
     // show random dice with src
     diceEl.src = `dice-${dice}.png`
+
+    //when dice is not one
+    if(dice !== 1){
+      currentScore += dice;
+      current0El.textContent = currentScore;
+    }else{
+     //next player..
+     
+    }
 });
