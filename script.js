f018b31c2bb9f7f4906c0994e04aabf2749444f7
Mari Azh <Marzi.aghchay@gmail.com>
2023-01-30 22:04:09 +0000

    use toggle for remove or add player cative

--- script.js
+++ script.js
@@ -36,5 +36,7 @@ btnRoll.addEventListener("click",function(){
      document.getElementById(`current--${activePlayer}`).textContent = 0;
      currentScore = 0;
     activePlayer = activePlayer === 0 ?1 :0;
+    player0El.classList.toggle("player--active")
+    player1El.classList.toggle("player--active")
     }
 });
