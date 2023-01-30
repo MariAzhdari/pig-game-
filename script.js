40efe53bccb852fcca0b1b7c58151aca923460a1
Mari Azh <Marzi.aghchay@gmail.com>
2023-01-30 13:48:50 +0000

    added some js like var and some conditions like hidden classList

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 'use strict';
+//selections elements
+let score0El = document.getElementById("score--0");
+let score1El = document.getElementById("score--1");
+let diceEl = document.querySelector(".dice")
+
+
+//conditions
+score0El.innerText = 0;
+score1El.innerText = 0;
+diceEl.classList.add("hidden")
