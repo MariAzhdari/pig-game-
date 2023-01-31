8f3b2b8dffdc7f150beebcd163bc38400760116c
Mari Azh <Marzi.aghchay@gmail.com>
2023-01-31 12:58:59 +0000

    initializing function

--- script.js
+++ script.js
@@ -11,15 +11,28 @@ let btnNew = document.querySelector(".btn--new");
 let btnRoll = document.querySelector(".btn--roll");
 let btnHold = document.querySelector(".btn--hold");
 
+let score,currentScore,activePlayer,playing;
+//initialize function
+let init =function(){
 
-//conditions
-score0El.innerText = 0;
-score1El.innerText = 0;
-diceEl.classList.add("hidden")
-const score =[0,0];
-let currentScore = 0;
-let activePlayer = 0;
-let playing = true;
+  score =[0,0];
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
+
+  score0El.textContent = 0;
+  score1El .textContent = 0;
+  current0El .textContent = 0;
+  current1El .textContent = 0;
+
+  diceEl.classList.add("hidden")
+  player0El.classList.remove('player--winner');
+  player0El.classList.remove('player--winner');
+  player0El.classList.add('player--active');
+  player0El.classList.remove('player--active');
+}
+ init()
+
 let switchPlayer = function(){
        document.getElementById(`current--${activePlayer}`).textContent = 0;
      currentScore = 0;
@@ -65,14 +78,5 @@ document.querySelector(`.player--${activePlayer}`).classList.add('player--winner
 } 
 }})
 
-btnNew.addEventListener('click',function(){
-  score0El.textContent = 0;
-  score1El .textContent = 0;
-  current0El .textContent = 0;
-  current1El .textContent = 0;
-  player0El.classList.remove('player--winner');
-  player0El.classList.remove('player--winner');
-  player0El.classList.add('player--active');
-  player0El.classList.remove('player--active');
+btnNew.addEventListener('click',init)
 
-})
